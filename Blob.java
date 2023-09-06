d226f35eaa564b8d960a84d9661b412494bfa0b1
Markus George <markusgeorge1207@gmail.com>
2023-09-06 13:56:40 -0700

    Objects folder created

--- Blob.java
+++ Blob.java
@@ -7,9 +7,25 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.nio.file.*;
 
 public class Blob 
 {
+
+    public static void main(String[] args) {
+        String inputFile = "1-1000.txt";
+        String outputFolder = "objects";
+
+        try {
+            String hash = Blob.createBlob(inputFile, outputFolder);
+            System.out.println("SHA-1 Hash: " + hash);
+            System.out.println("Blob file created in " + outputFolder);
+        } catch (IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+
     public static String calculateSHA1(String filePath) throws IOException, NoSuchAlgorithmException {
         MessageDigest sha1Digest = MessageDigest.getInstance("SHA-1");
         try (InputStream fileInputStream = new FileInputStream(filePath)) {
@@ -31,11 +47,11 @@ public class Blob
 
         return hexString.toString();
     }
-    public static byte[] readFile (File file) throws IOException
+    public static byte[] readFile (String inputFile) throws IOException
     {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 
-        try (FileInputStream files = new FileInputStream (file))
+        try (FileInputStream files = new FileInputStream (inputFile))
         {
             byte [] br = new byte [1024];
             int bytesRead;
@@ -46,11 +62,19 @@ public class Blob
         }
         return bytes.toByteArray();
     }
-private static void writeFile (String fileName, byte[] data) throws IOException
+public static String createBlob (String inputFile, String outputFolder) throws IOException, NoSuchAlgorithmException
 {
-    try (FileOutputStream fos = new FileOutputStream(fileName))
-    {
-        fos.write (data);
-    }
+byte [] data = readFile (inputFile);
+String hash = calculateSHA1(new String (data));
+
+File folder = new File (outputFolder);
+
+if (!folder.exists())
+{
+    folder.mkdir();
+}
+String outputFile = outputFolder + File.separator + hash;
+Files.write (Paths.get (outputFile), data);
+    return hash;
 }
 }
